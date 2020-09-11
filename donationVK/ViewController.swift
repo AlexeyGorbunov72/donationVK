cf90de4dc90c1979571e1ceb5287e86e04bc48e6
AnyDodic <korri.13@inbox.ru>
2020-09-11 17:09:30 +0300

    Initial Commit

---
+++ donationVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  donationVK
+//
+//  Created by Alexey on 11.09.2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
